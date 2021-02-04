27be6edd8d320a4f380262188ecfc7deeb11386d
Nikita Volkhonskiy <literature198@gmail.com>
2021-02-04 12:41:51 +0400

    feat:add alfa version of renderInitialCards for render all cards

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,36 @@ const currentUserName = document.querySelector('.user-profile__name');
 const currentUserJob = document.querySelector('.user-profile__description');
 
   // like variables
-const likeButtons = document.querySelectorAll('.cards-item__like-button');
+let likeButtons = document.querySelectorAll('.cards-item__like-button');
+
+  // initial cards variables
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 
 // Functions
 // user profile popup functions
@@ -42,11 +71,60 @@ function editProfileFormSubmitHandler(event) {
 // like button function
 function setLikeButton(event) {
   if (event.target.classList.contains('cards-item__like-button_active')) {
-    event.target.classList.toggle('cards-item__like-button_active')
+    event.target.classList.toggle('cards-item__like-button_active');
   } else {
-    event.target.classList.toggle('cards-item__like-button_active')
+    event.target.classList.toggle('cards-item__like-button_active');
   }
 }
+
+// renders cards on start function
+function renderInitialCards(arrayOfCards) {
+  let cardsContainer = document.querySelector('.cards__list');
+  let cardItemTemplate = document.querySelector('.template-card-item').content;
+
+  let cardElement = cardItemTemplate.querySelector('.cards-item').cloneNode(true);
+  //! вариант 1 = через for (рабочий)
+  for (let i = 0; i < arrayOfCards.length; i++) {
+    const { name, link } = arrayOfCards[i];
+    cardElement.querySelector('.cards-item__image').src = link;
+    cardElement.querySelector('.cards-item__image').alt = name;
+    cardElement.querySelector('.cards-item__title').textContent = name;
+    cardElement.querySelector('.cards-item__like-button').addEventListener('click', setLikeButton); // альтернативное решение с лайками (вроде самое адекватное)
+
+    cardsContainer.prepend(cardElement);
+  }
+
+
+  //! вариант 2 = через map (рабочий)
+  // let readyCards = arrayOfCards.map( (card) => {
+  //   let cardElement = cardItemTemplate.querySelector('.cards-item').cloneNode(true);
+  //   cardElement.querySelector('.cards-item__image').src = card.link;
+  //   cardElement.querySelector('.cards-item__image').alt = card.name;
+  //   cardElement.querySelector('.cards-item__title').textContent = card.name;
+
+  //   // event listener for like-button
+  //   cardElement.querySelector('.cards-item__like-button').addEventListener('click', setLikeButton); // альтернативное решение с лайками (вроде самое адекватное)
+
+  //   return cardElement;
+  // })
+  // cardsContainer.prepend(...readyCards);
+  // likeButtons = document.querySelectorAll('.cards-item__like-button'); // решение для лайков, скорее всего плохое
+
+
+  //! вариант 3 = через forEach (рабочий)
+  // arrayOfCards.forEach( (card) => {
+  //   let cardElement = cardItemTemplate.querySelector('.cards-item').cloneNode(true);
+  //   cardElement.querySelector('.cards-item__image').src = card.link;
+  //   cardElement.querySelector('.cards-item__image').alt = card.name;
+  //   cardElement.querySelector('.cards-item__title').textContent = card.name;
+  //   cardElement.querySelector('.cards-item__like-button').addEventListener('click', setLikeButton); // альтернативное решение с лайками (вроде самое адекватное)
+
+  //   cardsContainer.prepend(cardElement);
+  // })
+
+}
+renderInitialCards(initialCards);
+
 
 // event listeners
   // event listeners for user profile popup
@@ -54,7 +132,9 @@ editProfileOpenButton.addEventListener('click', openPopupEditUserProfile);
 editProfileCloseButton.addEventListener('click', closePopupEditUserProfile);
 editProfileForm.addEventListener('submit', editProfileFormSubmitHandler);
 
-  // event listeners for like-buttons
-likeButtons.forEach( (button) => {
-  button.addEventListener('click', setLikeButton)
-})
+  //! event listeners for like-buttons -- убрал, так как уже не актуально стало (вешаем при рендеринге и создании карточек этого слушателя)
+// likeButtons.forEach( (button) => {
+//   button.addEventListener('click', setLikeButton)
+// })
+
+

b17d9df91e8023148362b611c6abd2ed21dd122f
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-24 13:57:08 +0400

    feat: configure constructor function for PopupWithImage class

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -1,8 +1,13 @@
 import Popup from './Popup.js';
 class PopupWithImage extends Popup {
-  constructor() {
+  constructor(popupSelector, closeButtonSelector, imageSelector, titleSelector) {
+    super(popupSelector, closeButtonSelector);
 
+    this._image = this._popup.querySelector(imageSelector);
+    this._imageName = this._popup.querySelector(titleSelector);
   }
+
+
 }
 
 export default PopupWithImage;

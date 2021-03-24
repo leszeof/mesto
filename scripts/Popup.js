d5b86439f6986bc6e0544cd8ef069a2ac19af9ff
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-24 12:16:10 +0400

    feat: add to Popup class setEventListeners function and configure it

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -3,6 +3,7 @@ class Popup {
     this._popupElem = document.querySelector(popupSelector);
   }
 
+  // open popup
   open() {
     this._popupElem.classList.add('popup_opened');
 
@@ -10,6 +11,7 @@ class Popup {
     document.addEventListener('keydown', this._handleEscClose);
   }
 
+  // close popup
   close() {
     this._popupElem.classList.remove('popup_opened');
 
@@ -17,6 +19,7 @@ class Popup {
     document.removeEventListener('keydown', this._handleEscClose);
   }
 
+  // close popup on 'escape' press
   _handleEscClose(event) {
     if (event.key === 'Escape') {
       const curentOpenedPopup = document.querySelector('.popup_opened');
@@ -24,7 +27,19 @@ class Popup {
     }
   }
 
-  setEventListeners() {}
+  //! а что делать с закрытием по оверлею?
+
+  setEventListeners() {
+    // close popup on overlay click listener
+    this._popupElem.addEventListener('click', this._closeOnOverlayClick);
+
+    // close popup on close button
+    this._popupElem.querySelector('.popup__close-button').addEventListener('click', this.close);
+  }
+
+  _closeOnOverlayClick() {
+
+  }
 }
 
 export default Popup;

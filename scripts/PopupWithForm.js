72feaf2bd7830167c96bd7c2868d8f25ebcfc925
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-24 15:52:54 +0400

    feat: add and configure _getInputValues function

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,15 +1,25 @@
 import Popup from './Popup.js';
 class PopupWithForm extends Popup {
-  constructor({popupSelector, closeButtonSelector, formSelector, submitButtonSelector}, submitFormHandler) {
+  constructor({popupSelector, closeButtonSelector, formSelector, submitButtonSelector, formInputSelector}, submitFormHandler) {
     super(popupSelector, closeButtonSelector);
+
     this._popupForm = this._popupElem.querySelector(formSelector);
     this._popupSubmitButton = this._popupElem.querySelector(submitButtonSelector);
+    this._inputList = this._popupElem.querySelectorAll(formInputSelector);
 
     this._submitFormHandler = submitFormHandler;
   }
 
   // получаем значения всех полей формы
-  _getInputValues() {}
+  _getInputValues() {
+    this._formValues = {};
+
+    this._inputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+    });
+
+    return this._formValues;
+  }
 
 
   // обработчик сабмита

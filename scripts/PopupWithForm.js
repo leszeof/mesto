27690af96b4e73680bdb2d395e2c4a4a829126e8
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-24 15:48:46 +0400

    feat: configure PopupWithForm constructor

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,9 +1,22 @@
 import Popup from './Popup.js';
 class PopupWithForm extends Popup {
-  constructor(popupSelector, closeButtonSelector,) {
+  constructor({popupSelector, closeButtonSelector, formSelector, submitButtonSelector}, submitFormHandler) {
     super(popupSelector, closeButtonSelector);
+    this._popupForm = this._popupElem.querySelector(formSelector);
+    this._popupSubmitButton = this._popupElem.querySelector(submitButtonSelector);
+
+    this._submitFormHandler = submitFormHandler;
   }
 
+  // получаем значения всех полей формы
+  _getInputValues() {}
+
+
+  // обработчик сабмита
+  setEventListeners() {}
+
+  // делать ресет формы
+  close() {}
 }
 
 export default PopupWithForm;

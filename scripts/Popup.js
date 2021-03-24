3c4ea4ca663b49d4ad9b3b4202780ae7d6fbb802
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-24 11:05:06 +0400

    feat: add all basic skeleton for functions to Popup class

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -2,4 +2,12 @@ class Popup {
   constructor(popupSelector) {
     this._popupElem = document.querySelector(popupSelector);
   }
+
+  open() {}
+
+  close() {}
+
+  _handleEscClose() {}
+
+  setEventListeners() {}
 }

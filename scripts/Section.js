3aff4df4f536c558d931c5be111e39ddd42a2c83
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-24 10:40:22 +0400

    feat: create and configure rendeItems function

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,9 +1,17 @@
 class Section {
   constructor({items, renderer}, containerSelector) {
-    this._items = items;
+    this._itemsArray = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
+
+  renderItems() {
+    this._itemsArray.forEach(item => {
+      this._renderer(item);
+    })
+  }
+
+
 
 }
 

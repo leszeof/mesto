e182ddb1d97b674dded6781670b71f01870b8843
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-25 20:55:22 +0400

    feat: add and configure UserInfo getUserInfo function

--- scripts/UserInfo.js
+++ scripts/UserInfo.js
@@ -3,4 +3,13 @@ class UserInfo {
     this._userNameElem = document.querySelector(userNameSelector);
     this._userDescriptionElem = document.querySelector(userDescriptionSelector);
   }
+
+  getUserInfo() {
+    return {
+      currentUserName: this._userNameElem.value,
+      currentUserDescription: this._userDescriptionElem.value
+    }
+  }
+
+  setUserInfo() {}
 }

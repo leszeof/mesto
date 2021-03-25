4f2cfd91a3b17ba68aeb1896562a128dc74ff1bb
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-25 11:52:47 +0400

    refactor: little comments refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 // Imports
+  // data
 import initialCards from './data/cards-data.js';
+  // classes
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
 import Popup from './Popup.js';
 import PopupWithImage from './PopupWithImage.js';
 import PopupWithForm from './PopupWithForm.js';
+import Section from './Section.js';
+  // constants
 import {
   validationSettings,
   editProfilePopupWindow,

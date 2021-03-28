eb659b642d7ca8a37dc75e9ba0b7ac87c61d4169
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-28 12:12:51 +0400

    refactor: delete unused constants and imports of those constants to index.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -38,29 +38,15 @@ export const validationSettings = {
   // user profile popup
 export const editProfilePopupWindow = document.querySelector('.popup_type_edit-profile');
 export const editProfileOpenButton = document.querySelector('.user-profile__edit-profile-button');
-export const editProfileCloseButton = editProfilePopupWindow.querySelector('.popup__close-button');
 export const editProfileForm = editProfilePopupWindow.querySelector('.popup__form');
 export const editProfileUserNameInput = editProfileForm.querySelector('.popup-form__input_type_name');
 export const editProfileUserJobInput = editProfileForm.querySelector('.popup-form__input_type_job');
-export const currentUserName = document.querySelector('.user-profile__name');
-export const currentUserJob = document.querySelector('.user-profile__description');
 
   // add new place popup
 export const newCardPopupWindow = document.querySelector('.popup_type_add-place');
 export const newPlacePopupOpenButton = document.querySelector('.user-profile__add-button');
-export const newPlacePopupCloseButton = newCardPopupWindow.querySelector('.popup__close-button');
+// export const newPlacePopupCloseButton = newCardPopupWindow.querySelector('.popup__close-button');
 export const newPlacePopupForm = newCardPopupWindow.querySelector('.popup__form');
-export const newPlaceInput = newPlacePopupForm.querySelector('.popup-form__input_type_place');
-export const newPlaceImageLinkInput = newPlacePopupForm.querySelector('.popup-form__input_type_link');
-
-  // image preview popup
-export const imagePreviewPopupWindow = document.querySelector('.popup_type_image-preview');
-export const imagePreviewCloseButton = imagePreviewPopupWindow.querySelector('.popup__close-button');
-export const imagePreview = document.querySelector('.popup__image');
-export const imageCaption = document.querySelector('.popup__image-caption');
-
-  // initial cards
-export const cardsContainer = document.querySelector('.cards__list');
 
 export const userProfileSelectors = {
   userNameSelector: '.user-profile__name',

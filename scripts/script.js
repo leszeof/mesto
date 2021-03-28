63a076b229076436dd71b898bef49bb35040a067
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-28 11:41:59 +0400

    refactor: make all callback helper functions and classes work correctly together

--- scripts/script.js
+++ scripts/script.js
@@ -34,13 +34,9 @@ import {
   userProfileSelectors,
 } from './constants.js';
 
-
-
-
-
-
-// Popup classes in use
-//   edit profile popup controller copy
+// Classes in use
+  // Popup classes
+  // edit profile popup controller copy
 const editProfilePopup = new PopupWithForm(
   '.popup_type_edit-profile',
   updateProfile
@@ -50,12 +46,10 @@ editProfilePopup.setEventListeners();
   // add new place popup controller copy
 const addNewPlacePopup = new PopupWithForm(
   '.popup_type_add-place',
-  addNewPlace
+  submitNewCardHandler
 );
 addNewPlacePopup.setEventListeners();
 
-
-//! вот это вроде работает по канонам
   // image preview popup controller copy
 const imagePreviewPopup = new PopupWithImage(
   '.popup_type_image-preview',
@@ -63,36 +57,29 @@ const imagePreviewPopup = new PopupWithImage(
 );
 imagePreviewPopup.setEventListeners();
 
-
-//! секция имени + описания юзера (хтмл)
+  // UserInfo class
 const userInfo = new UserInfo(userProfileSelectors);
 
-//! секция отрисовки карточек, экзепляр Section
+  // Section class
 const cardsSection = new Section(
   {
     items: initialCards,
-    renderer: createCard,
+    renderer: (cardData) => {
+      const newCardElement = createCard(cardData, '.cards-item', handleCardClick);
+      cardsSection.addItem(newCardElement);
+    },
   },
   '.cards__list'
 );
 
-
-
-
-
-
 // Functions
-//TODO новые функции, которые используются как связки между классами либо при открытии/закрытии
-//! работает при открытии модалки с данными юзера (подставляет данные)
-//edit user profile popup functions
   // set input values when opening edit profile popup
 function setInputValues({currentUserName, currentUserDescription}) {
   editProfileUserNameInput.value = currentUserName;
   editProfileUserJobInput.value = currentUserDescription;
 }
 
-//! функции колбэки под классы
-// callback function for editProfilePopup copy of PopupWithForm class
+// callback function on submit edit user profile form
 function updateProfile(formData) {
   const newInfo = {
     newName: formData['new-user-name'],
@@ -106,65 +93,25 @@ function updateProfile(formData) {
 function handleCardClick(name, link) {
   imagePreviewPopup.open(name, link);
 }
-//!
 
-
-
-
-
-
-
-
-
-// add new place popup functions
-  // add new card function
-
-//! функция колбэк, которая нужна будет для отрисовки новой карточки после PopupWithForm класса
-  //!addNewPlacePopup
-function addNewPlace(formData) {
+// add new card callback function on submit add new place form
+function submitNewCardHandler(formData) {
   const newCardData = {
     name: formData['new-place-name'],
     link: formData['new-place-link']
   };
-
-  //! либо так (так было раньше)
-  // const newCardElement = createCard(newCardData);
-
-  //! либо так
-  const card = new Card(newCardData, '.cards-item', handleCardClick);
-  const cardElement = card.generateCard();
-
-  cardsSection.addItem(cardElement);
+  const newCardElement = createCard(newCardData, '.cards-item', handleCardClick);
+  cardsSection.addItem(newCardElement);
 }
 
-//! функция колбэк, которая нужна будет для отрисовки новой карточки после PopupWithForm класса
 // callback function for cardsSection copy of Section class
 function createCard(rawCardItem) {
   const card = new Card(rawCardItem, '.cards-item', handleCardClick);
-  const cardElement = card.generateCard();
-
-  cardsSection.addItem(cardElement);
+  return card.generateCard();
 }
-
-
 
 // renders cards on start
 cardsSection.renderItems();
-
-
-//TODO на вечер 27.03
-/*
-1) разобраться как добавить новую карточку в начало, а не конец (при добавлении 1 новой через форму)
-2) почистить файл констант и файлы классов от коментов и неиспользуемых переменных
-3) создать файл utils.js со всеми функциями общего назначения (и листенерами?), посмотри теорию
-
-
-PS по моему это нормально, что новая карточка идет в конец!
-*/
-
-
-
-
 
 // Event listeners
   // Event listeners for user profile popup
@@ -178,19 +125,12 @@ editProfileCloseButton.addEventListener('click', () => {
   editProfilePopup.close();
 });
 
-
-
-
-
 // Event listeners for add place popup
   // open add place popup
 newPlacePopupOpenButton.addEventListener('click', () => {
   newPlacePopupFormValidator.disableSubmitButton(newPlacePopupForm);
   addNewPlacePopup.open();
 });
-
-
-
 
 // Validation
   // edit user profile form validator
@@ -200,3 +140,22 @@ editProfileFormValidator.enableValidation();
   // add new place form validator
 const newPlacePopupFormValidator = new FormValidator(validationSettings , newPlacePopupForm);
 newPlacePopupFormValidator.enableValidation();
+
+//TODO утро 28.03
+/*
+1) создать папку компонентс, туда закинуть все классы
+  -- поменять пути при импортах к ним
+
+2) Создать папку utils
+  -- засунуть туда файл констант + переменные
+  -- скорее всего туда же пойдет файл utils.js с функциями (почитай чеклист)
+
+3) почистить файл констант и файлы классов от коментов и неиспользуемых переменных
+
+4) возможно разобать папку ДАТА и исправить пути к ней (положить в utils)
+
+
+! В файле index.js должно остаться только создание классов и добавление некоторых обработчиков.
+
+
+*/

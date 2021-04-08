b92d051f9fd7347876b479c116847ca723865172
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-08 22:05:59 +0400

    feat: add deleteCardHandler for Card class and some minor fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,15 @@
 class Card {
-  constructor({name, link, likes, owner}, cardSelector, handleCardClick) {
-    this._name = name;
-    this._link = link;
-    this._owner = owner;
-    this._likes = likes;
+  constructor({cardData, cardSelector, handleCardPreview, deleteCardHandler}) {
+    console.log(arguments);
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._owner = cardData.owner;
+    this._likes = cardData.likes;
 
     this._cardSelector = cardSelector;
-    this._handleCardClick = handleCardClick;
+
+    this._handleCardPreview = handleCardPreview;
+    this._deleteCardHandler = deleteCardHandler;
   }
 
   // get html template
@@ -40,7 +43,7 @@ class Card {
     this._htmlCard.querySelector('.cards-item__image').addEventListener('click', (event) => {
       const imageName = event.target.alt;
       const imageLink = event.target.src;
-      this._handleCardClick(imageName, imageLink)
+      this._handleCardPreview(imageName, imageLink)
     });
 
     // like card listener
@@ -49,7 +52,19 @@ class Card {
     });
 
     // delete card listener
-    this._htmlCard.querySelector('.cards-item__delete-button').addEventListener('click', (event) => {
+    this._htmlCard.querySelector('.cards-item__delete-button').addEventListener('click', () => {
+      // nice animations
+      this._htmlCard.style.transition = '0.5s';
+      this._htmlCard.style.transform = 'scale(0, 0.3) rotate(360deg)';
+      this._htmlCard.style.opacity = '0';
+      setTimeout(() => {
+        this._htmlCard.remove();
+      }, 500);
+
+      // delete callback
+        //! в нем надо открывать попап (экземпляр класса) ХОТИТЕ УДАЛИТЬ
+        //! при клике ДА - проваливаемся в САБМИТ ХЭНДЛЕР для ПОПАПА!
+        //! в нем желательно иметь id карточки чтобы послать DELETE запрос на сервер
       this._deleteCardHandler();
     });
   }
@@ -61,14 +76,14 @@ class Card {
   }
 
     // delete card function
-  _deleteCardHandler(event) {
-    this._htmlCard.style.transition = '0.5s';
-    this._htmlCard.style.transform = 'scale(0, 0.3) rotate(360deg)';
-    this._htmlCard.style.opacity = '0';
-    setTimeout(() => {
-      this._htmlCard.remove();
-    }, 500);
-  }
+  // _deleteCardHandler() {
+  //   this._htmlCard.style.transition = '0.5s';
+  //   this._htmlCard.style.transform = 'scale(0, 0.3) rotate(360deg)';
+  //   this._htmlCard.style.opacity = '0';
+  //   setTimeout(() => {
+  //     this._htmlCard.remove();
+  //   }, 500);
+  // }
 }
 
 export default Card;

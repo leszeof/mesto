db3e0a6d3e334191564829ea2e8876570bd58905
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-09 17:49:50 +0400

    feat: configure open function for PopupWithConfirm class (with extra functionality)

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -7,13 +7,21 @@ class PopupWithConfirm extends Popup {
     this._submitFormHandler = submitFormHandler;
   }
 
+  open(cardToDelete, cardID) {
+    super.open();
+    console.log('tyt open PopupWithConfirm');
+
+    this._cardToDelete = cardToDelete;
+    this._cardID = cardID;
+  }
+
   setEventListeners() {
     super.setEventListeners();
 
     this._popupForm.addEventListener('submit', (event) => {
       event.preventDefault();
 
-      this._submitFormHandler();
+      this._submitFormHandler(this._cardToDelete, this._cardID);
       this.close();
     });
   }

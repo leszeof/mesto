773a88f93e5254f64196308e5f55659d1b6e5efa
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-09 22:24:10 +0400

    refactor: totaly re-construct Card class (add: _cardDataBinding func, _cardElementsBinding func, delete comments, deleteCard func)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,35 @@
 class Card {
-  constructor({cardData, cardSelector, handleCardPreview, deleteCardHandler}) {
+  constructor({cardData, cardTemplateSelector: cardSelector, currentUserId, handleCardPreview, deleteCardHandler, likeCardHandler}) {
+    // card data binding
+    this._cardDataBinding(cardData, currentUserId);
+
+    // card elements binding
+    this._cardElementsBinding(cardSelector);
+
+    // callback handlers for card actions
+    this._handleCardPreview = handleCardPreview;
+    this._deleteCardHandler = deleteCardHandler;
+    this._likeCardHandler = likeCardHandler;
+  }
+
+  _cardDataBinding(cardData, currentUserId) {
     this._name = cardData.name;
     this._link = cardData.link;
     this._likes = cardData.likes.length;
+    this._cardId = cardData._id;
+    this._ownerId = cardData.owner._id;
+    this._isOwner = currentUserId === this._ownerId;
+  }
 
-    // пока не ясно нужно ли это
-    this._cardID = cardData._id;
-    this._ownerID = cardData.owner._id;
-
+  _cardElementsBinding(cardSelector) {
     this._cardSelector = cardSelector;
 
-    this._handleCardPreview = handleCardPreview;
-    this._deleteCardHandler = deleteCardHandler;
+    this._htmlCard = this._getTemplate();
+    this._cardImage = this._htmlCard.querySelector('.cards-item__image');
+    this._cardTitle = this._htmlCard.querySelector('.cards-item__title');
+    this._likeButton = this._htmlCard.querySelector('.cards-item__like-button');
+    this._cardLikeCouner = this._htmlCard.querySelector('.cards-item__like-counter');
+    this._cardDeleteButton = this._htmlCard.querySelector('.cards-item__delete-button');
   }
 
   // get html template
@@ -19,79 +37,64 @@ class Card {
     const cardElement = document
       .querySelector('.template-card-item')
       .content
-      .querySelector('.cards-item')
+      .querySelector(this._cardSelector)
       .cloneNode(true);
 
     return cardElement;
   }
 
-  // getCardID() {
-  //   return this._cardID;
-  // }
-
   // generate a card
   generateCard() {
-    this._htmlCard = this._getTemplate();
-
-    this._htmlCard.querySelector('.cards-item__title').textContent = this._name;
-    this._htmlCard.querySelector('.cards-item__image').alt = this._name;
-    this._htmlCard.querySelector('.cards-item__image').src = this._link;
-    this._htmlCard.querySelector('.cards-item__like-counter').textContent = this._likes;
+    this._renderCard();
 
     this._setEventListeners();
 
     return this._htmlCard;
   }
 
+  // render card presentation
+  _renderCard() {
+    this._cardTitle.textContent = this._name;
+    this._cardImage.alt = this._name;
+    this._cardImage.src = this._link;
+    this._cardLikeCouner.textContent = this._likes;
+  }
+
   // event listeners on a card
   _setEventListeners() {
-    // fill and open image preview popup
-    this._htmlCard.querySelector('.cards-item__image').addEventListener('click', (event) => {
-      const imageName = event.target.alt;
-      const imageLink = event.target.src;
+    // card image listener (preview popup)
+    this._cardImage.addEventListener('click', () => {
+      const imageName = this._cardImage.alt;
+      const imageLink = this._cardImage.src;
       this._handleCardPreview(imageName, imageLink)
     });
 
-    // like card listener
-    this._htmlCard.querySelector('.cards-item__like-button').addEventListener('click', (event) => {
-      this._setLikeHandler(event);
+    // like button listener
+    this._likeButton.addEventListener('click', (event) => {
+      this._like();
     });
 
-    // delete card listener
-    this._htmlCard.querySelector('.cards-item__delete-button').addEventListener('click', () => {
-      // delete callback
-        //! в нем надо открывать попап (экземпляр класса) ХОТИТЕ УДАЛИТЬ
-        //! при клике ДА - проваливаемся в САБМИТ ХЭНДЛЕР для ПОПАПА!
-          // сабмит хэндлер уже будет идти в АПИ класс, а затем удалять карточку
-        //! в нем желательно иметь id карточки чтобы послать DELETE запрос на сервер
-      this._deleteCardHandler(this, this._cardID);
-
-      // nice animations for delete
-        //! как вариант засунуть в отдельную публичную функцию и вызывать при реальном удалении карточки
-      // this._htmlCard.style.transition = '0.5s';
-      // this._htmlCard.style.transform = 'scale(0, 0.3) rotate(360deg)';
-      // this._htmlCard.style.opacity = '0';
-      // setTimeout(() => {
-      //   this._htmlCard.remove();
-      // }, 500);
+    // delete button listener
+    this._cardDeleteButton.addEventListener('click', () => {
+      this._deleteCardHandler(this, this._cardId);
     });
   }
 
   // Handlers functions
     // like card function
-  _setLikeHandler(event) {
-    event.target.classList.toggle('cards-item__like-button_active');
+  _like() {
+    //! логику надо менять
+    this._likeButton.classList.toggle('cards-item__like-button_active');
   }
 
-    // delete card function
-  // _deleteCardHandler() {
-  //   this._htmlCard.style.transition = '0.5s';
-  //   this._htmlCard.style.transform = 'scale(0, 0.3) rotate(360deg)';
-  //   this._htmlCard.style.opacity = '0';
-  //   setTimeout(() => {
-  //     this._htmlCard.remove();
-  //   }, 500);
-  // }
+  deleteCard() {
+    this._htmlCard.style.transition = '0.5s';
+    this._htmlCard.style.transform = 'scale(0, 0.3) rotate(360deg)';
+    this._htmlCard.style.opacity = '0';
+    setTimeout(() => {
+      this._htmlCard.remove();
+    }, 500);
+  }
 }
 
 export default Card;
